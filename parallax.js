8022b865d591b4f5f66974b27fcfb24078b433c6
studio-afterglow <47209883+studio-afterglow@users.noreply.github.com>
2019-05-29 23:17:55 +0200

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -4,12 +4,14 @@ $(window).scroll(function() {
 
 function paralaksa(){
     var wScroll = $(window).scrollTop();
+    var media = window.matchMedia("screen and (min-width: 1300px)");
     
     if(wScroll < 210){
     $('.banner-element').css('padding-top', 0+(wScroll*0.5)+'px')}
     
+    if(media.maches){
     $('.menu-grid').css('opacity', (wScroll*0.0016))
     $('.part-1').css('opacity', (wScroll*0.003)-3.3)
     $('.news').css('opacity', (wScroll*0.005)-8.31)
-    $('.offer-grid').css('opacity', (wScroll*0.005)-11.4)
+    $('.offer-grid').css('opacity', (wScroll*0.005)-11.4)}
 }
